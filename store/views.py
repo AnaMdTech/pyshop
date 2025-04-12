15d93f7e814ca260c70cc6ec70be2a20a0bc9072
Anamd-hub <anasmdl120@gmail.com>
2025-04-12 23:59:18 +0300

    Product Detail Page

--- store/views.py
+++ store/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 def home(request):
     products = Product.objects.all()  # Fetch all products from the DB
     return render(request, 'store/home.html', {'products': products})
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'store/product_detail.html', {'product': product})
